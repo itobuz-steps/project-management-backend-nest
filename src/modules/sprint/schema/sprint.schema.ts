8fc0b19d05059979494645ef8ced6146e1a15daf
Devjyoti Banerjee <devjyoti.banerjee@itobuz.com>
2026-02-04 13:27:31 +0530

    PM-310 feat: sprint schema

---
+++ src/modules/sprint/schema/sprint.schema.ts
@@ -0,0 +1,32 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
+
+@Schema({ timestamps: true })
+export class Sprint extends Document {
+  @Prop({ required: true })
+  key: string;
+
+  @Prop({
+    type: [{ type: Types.ObjectId, ref: 'Task' }],
+    default: [],
+  })
+  tasks: Types.ObjectId[];
+
+  @Prop()
+  dueDate?: Date;
+
+  @Prop({ default: false })
+  isCompleted: boolean;
+
+  @Prop({ default: 0 })
+  storyPoint: number;
+
+  @Prop({
+    type: Types.ObjectId,
+    ref: 'Project',
+    required: true,
+  })
+  projectId: Types.ObjectId;
+}
+
+export const SprintSchema = SchemaFactory.createForClass(Sprint);

edf412502d8761006b280e5f3aaef1b2d925e84c
Sujal Gupta <sujal.gupta@itobuz.com>
2026-02-04 16:16:05 +0530

    PM-307 chore: get port from config instead of process.env

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -20,7 +21,11 @@ async function bootstrap() {
     SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api', app, documentFactory);
 
-  await app.listen(process.env.PORT as string | number);
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT', 3000);
+
+  await app.listen(port);
+
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap().catch((err) => {

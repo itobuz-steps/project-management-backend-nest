f78634d32a3f4a62d676d1f57636a0273da10ed5
Devjyoti Banerjee <devjyoti.banerjee@itobuz.com>
2026-02-04 18:38:06 +0530

    PM-313 feat: integrating auth middleware in sprint module

--- src/modules/sprint/sprint.module.ts
+++ src/modules/sprint/sprint.module.ts
@@ -4,6 +4,8 @@ import { Sprint, SprintSchema } from './schema/sprint.schema';
 import { Project, ProjectSchema } from '../project/schema/project.schema';
 import { SprintService } from './sprint.service';
 import { SprintController } from './sprint.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports: [
@@ -11,8 +13,11 @@ import { SprintController } from './sprint.controller';
       { name: Sprint.name, schema: SprintSchema },
       { name: Project.name, schema: ProjectSchema },
     ]),
+    JwtModule.register({}),
+    AuthModule,
   ],
   controllers: [SprintController],
   providers: [SprintService],
+  exports: [SprintService],
 })
 export class SprintModule {}

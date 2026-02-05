cd74b0d7ce222a252515fba9006e2d382f0660fc
Sujal Gupta <sujal.gupta@itobuz.com>
2026-02-05 15:11:42 +0530

    PM-318 fix: change refresh-token route from post to get

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -7,6 +7,7 @@ import {
   Req,
   UnauthorizedException,
   UseGuards,
+  Get,
 } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import * as bcrypt from 'bcrypt';
@@ -192,7 +193,7 @@ export class AuthController {
     }
   }
 
-  @Post('refresh-token')
+  @Get('refresh-token')
   @UseGuards(IsAuthenticated)
   refreshToken(@Req() req: AuthenticatedRequest) {
     try {

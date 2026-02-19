2b93e47f7b7d9ca70b1baed558294809be14d8f9
Sujal Gupta <sujal.gupta@itobuz.com>
2026-02-19 14:20:46 +0530

    fix: PM-329 story point type in create task dto

--- src/modules/tasks/dto/create-task.dto.ts
+++ src/modules/tasks/dto/create-task.dto.ts
@@ -5,8 +5,8 @@ import {
   IsEnum,
   IsArray,
   IsDateString,
-  IsNumber,
   IsMongoId,
+  IsNumberString,
 } from 'class-validator';
 import { TASK_PRIORITIES, TASK_TYPES } from '../../../constants/task.constants';
 import type { TaskPriority, TaskType } from '../../../constants/task.constants';
@@ -94,7 +94,7 @@ export class CreateTaskDto {
     required: false,
   })
   @IsOptional()
-  @IsNumber()
+  @IsNumberString()
   storyPoint?: number;
 
   @ApiProperty({

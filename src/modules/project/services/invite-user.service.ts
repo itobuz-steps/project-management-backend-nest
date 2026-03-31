a00052780ac94d054df104c0cf7b2d35853670ed
Shaswata Biswas <shaswata.biswas@itobuz.com>
2026-03-31 12:11:33 +0530

    fix: PM-88 updated invite user functionality route in email

--- src/modules/project/services/invite-user.service.ts
+++ src/modules/project/services/invite-user.service.ts
@@ -88,6 +88,7 @@ export class InviteUserService {
   }
 
   async acceptUsersInvite(token: string | undefined, user?: UserDocument) {
+    console.log('accepted token route');
     if (!token) {
       throw new BadRequestException('No token provided');
     }
@@ -117,6 +118,9 @@ export class InviteUserService {
     }
 
     const { email: inviteEmail, projectId } = decodedInvite;
+
+    console.log('email', inviteEmail);
+    console.log('projectId', projectId);
 
     if (user.email !== inviteEmail) {
       throw new ForbiddenException(
